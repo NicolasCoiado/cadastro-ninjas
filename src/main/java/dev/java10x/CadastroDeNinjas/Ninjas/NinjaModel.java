740493beebc0c2b5d039613293acfe594c1ebb10
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-01-29 13:54:36 -0300

    feat: Migrations para tb_cadastro incluindo a column de rank

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -27,8 +27,8 @@ public class NinjaModel {
     @Column(name = "img_url")
     private String imgUrl;
 
-    @Column(name = "rank")
-    private String rank;
+    // @Column(name = "rank")
+    // private String rank;
 
     @Column(name = "idade")
     private int idade;
