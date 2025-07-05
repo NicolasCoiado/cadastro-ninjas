7afa6656769b379b1d4fc5b1d206d3938c5f2ac3
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-07-05 13:23:38 -0300

    feat: Cria as rotas de missoes com mock de dados.

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,31 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping ("missoes")
 public class MissoesController {
+    //GET
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas com sucesso.";
+    }
+
+    //POST
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso.";
+    }
+
+    //PUT
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso.";
+    }
+
+    //DELETE
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso.";
+    }
 }
