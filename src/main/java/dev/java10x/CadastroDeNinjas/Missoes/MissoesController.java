2e663d1e5a3da02156887e439f7e6b1446f5fc19
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-07-07 17:30:55 -0300

    feat: Implementa as rotas de cadastro, listagem, e deleção de Missões.

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,19 +2,33 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping ("/missoes")
 public class MissoesController {
+
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     //GET
     @GetMapping("/listar")
-    public String listarMissoes(){
-        return "Missoes listadas com sucesso.";
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
+    }
+
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissaoPorId(@PathVariable Long id){
+        return missoesService.listarMissaoPorId(id);
     }
 
     //POST
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "Missao criada com sucesso.";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
+        return missoesService.criarMissao(missao);
     }
 
     //PUT
@@ -24,8 +38,9 @@ public class MissoesController {
     }
 
     //DELETE
-    @DeleteMapping("/deletar")
-    public String deletarMissao(){
-        return "Missao deletada com sucesso.";
+    @DeleteMapping("/deletar/{id}")
+    public String deletarMissao(@PathVariable Long id){
+        missoesService.deletarMissao(id);
+        return "Missao deletada com sucesso!";
     }
 }
